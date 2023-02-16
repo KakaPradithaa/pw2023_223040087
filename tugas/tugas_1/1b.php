612df0398b978cc6a458d6098dae26b6df7cab4a
KakaPradithaa <kakapradithaa123@gmail.com>
2023-02-16 17:39:11 +0700

    mengerjakan tugas 1

---
+++ tugas/tugas_1/1b.php
@@ -0,0 +1,21 @@
+<?php 
+$npm="87"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    Aku adalah <?php echo $npm ?><br>
+    Jika aku dikali 5, maka aku akan menjadi <?php echo $npm * 5 ?><br>
+    Jika aku dibagi 2, maka aku akan menjadi <?php echo 435 / 2 ?><br>
+    Jika aku ditambah 75, maka aku akan menjadi <?php echo 217.5 + 75 ?><br>
+    Jika aku dikurang 20, maka aku akan menjadi <?php echo 292.5 - 20 ?>
+    </p>
+</body>
+</html>
